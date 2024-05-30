4aa7cf11e134cdcae9b45c7ab805f6d4fb91dde5
Casto MALIMALI <castomalimali@gmail.com>
2024-05-30 19:31:05 +0300

    Push Controller

--- resources/views/partials/contacts-book.blade.php
+++ resources/views/partials/contacts-book.blade.php
@@ -15,27 +15,29 @@
                 <div class="consultation_content" data-aos="fade-up">
                     <h6>Get in touch</h6>
                     <h2 class="text-white">We're here to help! Feel free to get in touch with us at any time.</h2>
-                    <form id="contactpage" method="post" class="position-relative" action="{{ route('contact.store') }}">
+
+                    <form id="contactpage" method="POST" action="{{ route('contact.store') }}" class="position-relative">
                         @csrf
                         <div class="form-group input1 float-left">
-                            <input type="text" class="form_style" placeholder="Name" name="fname" id="fname">
+                            <input type="text" class="form_style" placeholder="Name" name="fname" id="fname" required>
                         </div>
                         <div class="form-group float-left">
-                            <input type="tel" class="form_style" placeholder="Phone" name="phone" id="phone">
+                            <input type="tel" class="form_style" placeholder="Phone" name="phone" id="phone" required>
                         </div>
                         <div class="form-group input1 float-left">
-                            <input type="email" class="form_style" placeholder="Email" name="email" id="email">
+                            <input type="email" class="form_style" placeholder="Email" name="email" id="email" required>
                         </div>
                         <div class="form-group float-left">
-                            <select class="form-control">
-                                <option>Community</option>
-                                <option>Career Guidance</option>
-                                <option>Consultation</option>
-                                <option>Others</option>
+                            <select class="form-control" name="type" required>
+                                <option value="" disabled selected>Select Type</option>
+                                <option value="Community">Community</option>
+                                <option value="Career Guidance">Career Guidance</option>
+                                <option value="Consultation">Consultation</option>
+                                <option value="Others">Others</option>
                             </select>
                         </div>
                         <div class="form-group message">
-                            <textarea class="form_style" placeholder="Message" rows="3" name="msg"></textarea>
+                            <textarea class="form_style" placeholder="Message" rows="3" name="msg" required></textarea>
                         </div>
                         <button id="submit" type="submit" class="appointment">Send Now<i class="fa-solid fa-arrow-right"></i></button>
                     </form>
